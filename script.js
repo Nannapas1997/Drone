0c97ad17be39eceae07f8408b92ec234b0dd30fc
Nannapas1997 <nannapas4869@gmail.com>
2024-10-18 17:38:27 +0700

    responsive first text overlay

--- script.js
+++ script.js
@@ -11,3 +11,45 @@ menuLinks.forEach(link => {
         this.classList.add('active');
     });   
 });     
+
+$(document).ready(function () {
+    // Function to wrap menu items when screen size is less than 1359px
+    function handleMenuResize() {
+      if ($(window).width() <= 1359) {
+        if (!$(".menu").length) {
+          // If the .menu span doesn't exist, create it and wrap all the navigation items
+          $("<span class='menu'></span>").appendTo(".nav-bar");
+          $(".nav-bar li:not(.logo), .button__group").appendTo(".menu");
+        }
+      } else {
+        if ($(".menu").length) {
+          // If the .menu span exists and screen size is greater, unwrap the items
+          $(".menu li, .button__group").insertBefore(".open-menu");
+          $(".menu").remove();
+        }
+      }
+    }
+
+    // Handle menu on page load
+    handleMenuResize();
+
+    // Handle menu on window resize
+    $(window).resize(function () {
+      handleMenuResize();
+    });
+    if ($(window).width() <= 1359) {
+    // Open the hamburger menu
+        $(".open-menu").click(function () {
+        $(".menu").addClass("menu-open");
+        $(".open-menu").hide(); // Hide hamburger icon
+        $(".close-menu").show(); // Show close icon
+        });
+
+        // Close the hamburger menu
+        $(document).on("click", ".close-menu", function () {
+        $(".menu").removeClass("menu-open");
+        $(".close-menu").hide(); // Hide close icon
+        $(".open-menu").show(); // Show hamburger icon
+        });
+    }
+  });
